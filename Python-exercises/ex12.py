df6556f3b823566955f790989fc5aaaf567bc115
NyoNyoHtwe <lovelynyo31@gmail.com>
2018-06-28 11:00:11 -0800

    Prompting and input()

---
+++ Python-exercises/ex12.py
@@ -0,0 +1,8 @@
+print ("How old are you?"),
+age= input("Enter your age")
+print ("How tall are you?"),
+height = input("Enter your height")
+print ("How much do you weight?"),
+weight = input("Enter your weight")
+print ("So, you're %r old, %r tall and %r heavy." % (age,height,weight))
+
